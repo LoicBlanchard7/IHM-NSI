6bfa2dff7b31d0afcc778ba4a2b5025f367c4ef9
BLANCHARD Loic <loic.blanchard9@etu.univ-lorraine.fr>
2024-03-27 20:21:44 +0100

    Application 2

---
+++ Exercice-Application-2/exercice2.js
@@ -0,0 +1,11 @@
+// Déclaration de la variable mot initialisée à "Numerique"
+
+
+// Définition de la fonction inverserMot
+
+
+// Définition de la fonction afficherMotInverse
+
+
+
+// Appel de la fonction inverserMot avec mot et affichage du mot inversé grâce à la fonction afficherMotInverse
