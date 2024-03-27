ba89c2fcc2f2faa3305a7178a5533b263c38e758
BLANCHARD Loic <loic.blanchard9@etu.univ-lorraine.fr>
2024-03-28 00:37:12 +0100

    TD1-IHM

---
+++ Exercice-TD1-IHM/script.js
@@ -0,0 +1,47 @@
+/*
+    Fonction exercice 1 : Changement de couleur
+*/
+// Créer les fonctions "changeBleu" et "changeRouge" en dessous.
+
+
+/*
+    Fonction exercice 2 : Inverser un mot
+*/
+// Créer la fonction "inverser()" en dessous.
+
+
+/*
+    Fonction exercice 3 : Calculatrice
+*/
+function addition() {
+    var nombre1 = parseFloat(document.getElementById('nombre1').value);
+    // A compléter
+}
+
+function soustraction() {
+    // A compléter
+}
+
+function multiplication() {    
+    // A compléter
+}
+
+function division() {   
+    // A compléter
+}
+
+function afficherResultat(resultat) {
+    // A compléter
+}
+
+/*
+    Fonction exercice 4 : Compteur de clic
+*/
+
+// Sélectionner le bouton et le compteur
+const button = document.getElementById('clicButton');
+const affichageClic = document.getElementById('clickCount');
+
+// Initialiser le compteur
+
+// Ajouter un écouteur d'événements au clic sur le bouton
