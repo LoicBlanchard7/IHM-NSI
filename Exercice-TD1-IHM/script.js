52b50fc48d907a7523357a4c122f4ed1d8f6f953
BLANCHARD Loic <loic.blanchard9@etu.univ-lorraine.fr>
2024-04-04 00:59:47 +0200

    Correction

--- Exercice-TD1-IHM/script.js
+++ Exercice-TD1-IHM/script.js
@@ -2,36 +2,67 @@
     Fonction exercice 1 : Changement de couleur
 */
 // Créer les fonctions "changeBleu" et "changeRouge" en dessous.
+function changeBleu() {
+    var monPara = document.querySelector('#para');
+    monPara.classList.add("bleu");
+    monPara.classList.remove("rouge");
+}
 
+function changeRouge() {
+    var monPara = document.querySelector('#para');
+    monPara.classList.add("rouge");
+    monPara.classList.remove("bleu");
+}
 
 /*
     Fonction exercice 2 : Inverser un mot
 */
 // Créer la fonction "inverser()" en dessous.
+function inverser(){
+    var mot = document.getElementById('mot').value;
+    console.log(mot)
+    var reponse = ""
+    for (var i = mot.length - 1; i>=0; i-- ) {
+        reponse += mot[i];
+    }
+    console.log(reponse)
 
+    document.getElementById('motInverse').innerHTML = "Votre mot inversé : " + reponse
+}
 
 /*
     Fonction exercice 3 : Calculatrice
 */
 function addition() {
     var nombre1 = parseFloat(document.getElementById('nombre1').value);
-    // A compléter
+    var nombre2 = parseFloat(document.getElementById('nombre2').value);
+    var resultat = nombre1 + nombre2;
+    afficherResultat(resultat);
 }
 
 function soustraction() {
-    // A compléter
+    var nombre1 = parseFloat(document.getElementById('nombre1').value);
+    var nombre2 = parseFloat(document.getElementById('nombre2').value);
+    var resultat = nombre1 - nombre2;
+    afficherResultat(resultat);
 }
 
-function multiplication() {    
-    // A compléter
+function multiplication() {
+    var nombre1 = parseFloat(document.getElementById('nombre1').value);
+    var nombre2 = parseFloat(document.getElementById('nombre2').value);
+    var resultat = nombre1 * nombre2;
+    afficherResultat(resultat);
 }
 
-function division() {   
-    // A compléter
+function division() {
+    var nombre1 = parseFloat(document.getElementById('nombre1').value);
+    var nombre2 = parseFloat(document.getElementById('nombre2').value);
+    var resultat = nombre1 / nombre2;
+    afficherResultat(resultat);
 }
 
 function afficherResultat(resultat) {
-    // A compléter
+    document.getElementById('resultat').innerHTML = "Résultat : " + resultat;
 }
 
 /*
